cf00d30d3d0455f491f830d8c6edf4d0ec6e192e
Truong Nguyen <nhattruong05022003@gmail.com>
2026-01-21 20:55:50 +0700

    Add test case for low power mode

--- bsp/!test/bsp_test.c
+++ bsp/!test/bsp_test.c
@@ -16,17 +16,17 @@ void abort(void)
  *********************************************************************************************************************/
 void bsp_ioport_test_case(void)
 {
-    BSP_IO_Configurate(BSP_IO_PORTA_PIN_0, BSP_IO_CONFIG_OUTPUT_GPIO_PUSH_PULL | BSP_IO_MODE_OUTPUT_50MHZ | BSP_IO_OUTPUT_INIT_STATE_HIGH);
+    BSP_IO_Configurate(BSP_IO_PORTA_PIN_2, BSP_IO_CONFIG_OUTPUT_GPIO_PUSH_PULL | BSP_IO_MODE_OUTPUT_50MHZ | BSP_IO_OUTPUT_INIT_STATE_HIGH);
     BSP_IO_Configurate(BSP_IO_PORTA_PIN_1, BSP_IO_CONFIG_INPUT_PULL_UP_DOWN | BSP_IO_INPUT_PULL_MODE_PULLHIGH);
-    ASSERT((GPIOA->CRL & 0x0F) == (BSP_IO_CONFIG_OUTPUT_GPIO_PUSH_PULL | BSP_IO_MODE_OUTPUT_50MHZ));
-    BSP_IO_Write(BSP_IO_PORTA_PIN_0, BSP_IO_STATE_LOW);
-    ASSERT((GPIOA->ODR & 0x01) == BSP_IO_STATE_LOW);
+    ASSERT(((GPIOA->CRL & (0x0FU << 8U)) >> 8U) == (BSP_IO_CONFIG_OUTPUT_GPIO_PUSH_PULL | BSP_IO_MODE_OUTPUT_50MHZ));
+    BSP_IO_Write(BSP_IO_PORTA_PIN_2, BSP_IO_STATE_LOW);
+    ASSERT(((GPIOA->ODR & 0x04U) >> 2U) == BSP_IO_STATE_LOW);
     ASSERT(BSP_IO_Read(BSP_IO_PORTA_PIN_1) == BSP_IO_STATE_LOW);
-    BSP_IO_Write(BSP_IO_PORTA_PIN_0, BSP_IO_STATE_HIGH);
-    ASSERT((GPIOA->ODR & 0x01) == BSP_IO_STATE_HIGH);
+    BSP_IO_Write(BSP_IO_PORTA_PIN_2, BSP_IO_STATE_HIGH);
+    ASSERT(((GPIOA->ODR & 0x04U) >> 2U) == BSP_IO_STATE_HIGH);
     ASSERT(BSP_IO_Read(BSP_IO_PORTA_PIN_1) == BSP_IO_STATE_HIGH);
-    BSP_IO_Toggle(BSP_IO_PORTA_PIN_0);
-    ASSERT((GPIOA->ODR & 0x01) == BSP_IO_STATE_LOW);
+    BSP_IO_Toggle(BSP_IO_PORTA_PIN_2);
+    ASSERT(((GPIOA->ODR & 0x04U) >> 2U) == BSP_IO_STATE_LOW);
     ASSERT(BSP_IO_Read(BSP_IO_PORTA_PIN_1) == BSP_IO_STATE_LOW);
 }
 
@@ -175,8 +175,24 @@ void bsp_heap_test_case(void)
 
 #endif
 
+void bsp_mcu_reset_test_case(void)
+{
+    if(PWR->CSR_b.WUF != 1U)
+    {
+        BSP_McuReset_ConfigWakeUpPin();
+        BSP_McuReset_LowPowerModeTrigger(BSP_LOW_POWER_MODE_STANDBY);
+        while(1);
+    }
+    else
+    {
+        BSP_McuReset_WakeUpFlagClear();
+        BSP_McuReset_DeconfigWakeUpPin();
+    }
+}
+
 void bsp_run_test(void)
 {
+    bsp_mcu_reset_test_case();
     bsp_ioport_test_case();
     bsp_irq_test_case();
     bsp_exception_test_case();
